8d47b99beac2c6967e0303f26e914af3f4377a2a
tuannguyenle <nltuannct230493@gmail.com>
2024-10-24 10:21:29 +0700

    Updates

---
+++ _nuxt/1u4U6LqS.js
@@ -0,0 +1,1 @@
+import{r as t}from"./Czc87scG.js";const a=()=>{const e=t("");return{walletAddress:e,setWalletAddress:s=>{e.value=s}}};export{a as u};
---
+++ _nuxt/Dqzb9Uy-.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./Czc87scG.js";const c={};function n(r,s,a,p,_,d){return o(),t("div",null," FAQ page ")}const i=e(c,[["render",n]]);export{i as default};
