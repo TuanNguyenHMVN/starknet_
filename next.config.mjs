d9bf49de02c640db54e62badcf0fa1c7d2d38931
Tuan Nguyen Le <tuan.nguyenle@dirox.net>
2024-10-25 21:01:41 +0700

    setup deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  output: "export",
+  // Other Next.js config options can go here
+};
+
+module.exports = nextConfig;
 
 export default nextConfig;
