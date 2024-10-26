fd55b48dedf61559ecbfb9e26078447b67a80b99
Tuan Nguyen Le <tuan.nguyenle@siliconprime.com>
2024-10-26 16:59:10 +0700

    hoempage

---
+++ app/faq/page.js
@@ -0,0 +1,9 @@
+export default function FaqPage() {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">
+        FAQs page
+      </h1>
+    </div>
+  );
+}
--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,18 @@
+'use client'; // Required for client components
+
+import { useRouter } from 'next/navigation';
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push('/staking'); // Navigate to the About page
+  };
   return (
-    <div className="container">
+    <div className="container text-center">
       <h1 className="text-center mt-5">
-        Welcome to Custom Next.js with Bootstrap!
+        Welcome to Stakestark_ Application!
       </h1>
-      <button className="btn btn-primary">Custom Button</button>
+      <button className="btn btn-primary" onClick={handleNavigate}>Start Staking</button>
     </div>
   );
 }
