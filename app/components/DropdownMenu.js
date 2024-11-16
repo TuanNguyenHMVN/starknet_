244bfa5c21cb6a67530aafbe83c693ab7a801b34
Tuan Nguyen Le <tuan.nguyenle@siliconprime.com>
2024-11-16 13:42:42 +0700

    update UI

--- app/components/DropdownMenu.js
+++ app/components/DropdownMenu.js
@@ -7,7 +7,7 @@ import styles from "../styles/DropdownMenu.module.scss";
 import { connect } from 'get-starknet'; // v4.0.0 min
 import { WalletAccount } from 'starknet';
 
-const DropdownMenu = () => {
+const DropdownMenu = ({isHomePage}) => {
   const {userWallet, updateWallet, setWalletAccount} = useStore();
 
   const [walletAddress, setWalletAddress] = useState('')
@@ -26,11 +26,22 @@ const DropdownMenu = () => {
     <div className={styles['menu-dropdown-icon']}>
       <Dropdown>
         <Dropdown.Toggle variant="link" className="text-decoration-none">
-          <i className={`${styles['menu-dropdown-icon']} bi bi-list dropdown-icon`}></i>
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={32}
+              height={32}
+              viewBox="0 0 32 32"
+              fill="none"
+            >
+              <path
+                d="M4 24V21.3333H28V24H4ZM4 17.3333V14.6667H28V17.3333H4ZM4 10.6667V8H28V10.6667H4Z"
+                fill="#0F0B46"
+              />
+          </svg>
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item>
+          {!isHomePage && <Dropdown.Item>
             {walletAddress && <div className={styles['account-info']}>
               {`${walletAddress.slice(0,5)}...${walletAddress.slice(-3)}`}
             </div>}
@@ -41,8 +52,8 @@ const DropdownMenu = () => {
             >
               Connect Wallet
             </Button>}
-          </Dropdown.Item>
-          <Dropdown.Divider />
+          </Dropdown.Item>}
+          {!isHomePage && <Dropdown.Divider />}
           <Dropdown.Item href="/home">Home</Dropdown.Item>
           <Dropdown.Item href="/staking">Staking</Dropdown.Item>
           <Dropdown.Item href="/faq">FAQs</Dropdown.Item>
--- app/components/Header.js
+++ app/components/Header.js
@@ -78,18 +78,7 @@ const Header = () => {
             </li>
           </ul>
           <div className="mobile-menu-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={32}
-              height={32}
-              viewBox="0 0 32 32"
-              fill="none"
-            >
-              <path
-                d="M4 24V21.3333H28V24H4ZM4 17.3333V14.6667H28V17.3333H4ZM4 10.6667V8H28V10.6667H4Z"
-                fill="#0F0B46"
-              />
-            </svg>
+            <DropdownMenu className={styles["dropdown-menu"]} />
           </div>
         </div>
       </nav>
